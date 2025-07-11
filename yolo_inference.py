98e2afa139239974034c71c84dadceda43b5903f
Long Thanh Tran <longttran98@gmail.com>
2025-07-10 22:16:52 -0500

    update gitignore

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,12 +1,13 @@
 from ultralytics import YOLO
 
-model= YOLO('yolov8x')
+# model= YOLO('models/yolov5_last.pt')
+model= YOLO('yolov8x.pt')
 
-result = model.predict('dataset/pic/2.png', save= True)
-# result = model.predict('dataset/matchrec.mp4', save=True)
+# result = model.predict('dataset/matchrec.mp4',conf=0.2, save= True)
+result = model.track('dataset/matchrec.mp4', conf=0.2, save=True, save_txt=True)
 
-print(result)
-print('boxes:')
+# print(result)
+# print('boxes:')
 
-for box in result[0].boxes:
-    print(box)
+# for box in result[0].boxes:
+#     print(box)
